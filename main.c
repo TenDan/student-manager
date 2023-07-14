7ebe1c03f08c2b1f392ff58208c7bc4f72bf3bb5
Maciej Ślusarz <mslusarz@student.agh.edu.pl>
2023-07-14 15:02:05 +0200

    Move prompt functions to separate header and source file

--- main.c
+++ main.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "record.h"
+#include "prompt.h"
 
 #define SEPARATOR ";\n"
-#define BUFFER 1000
-
-int print_prompt() {
-    printf(" > ");
-    char prompt[BUFFER];
-
-    if (fgets(prompt, BUFFER, stdin) == NULL) {
-        printf("Error occured. Try again. \n");
-        return -1;
-    }
-
-    char *command = strtok(prompt, " \n");
-
-    if (strcmp(command, "add") == 0) {
-        printf("Adding to database...\n");
-    } else if (strcmp(command, "read") == 0) {
-        printf("Reading from database...\n");
-    } else {
-        printf("Unknown command. Try again.\n");
-        return -1;
-    }
-
-    return 0;
-}
 
 int main(int argc, char **argv) {
     char *line = NULL;
---
+++ src/prompt.h
@@ -0,0 +1,12 @@
+#ifndef PROMPT_H_
+#define PROMPT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER 1000
+
+int print_prompt();
+
+#endif /* PROMPT_H_ */
