88215eb2bf93db816d2a437bf96063ffd83ad8fd
Maciej Ślusarz <mslusarz@student.agh.edu.pl>
2023-07-19 22:00:10 +0200

    Move reading db action to read_database function call

--- main.c
+++ main.c
@@ -11,9 +11,6 @@
 #define SEPARATOR ";\n"
 
 int main(int argc, char **argv) {
-    char *line = NULL;
-    size_t line_len = 0;
-
     if (argc != 2) {
         fprintf(stderr, "Valid usage: %s <database_file>\n", argv[0]);
         return 1;
@@ -43,32 +40,15 @@ int main(int argc, char **argv) {
 
     if (database == NULL) {
         perror("Error occured");
-        return 1;
+        return -1;
     }
 
-    int chars_read, line_num = 0;
-    while ((chars_read = getline(&line, &line_len, database)) != -1) {
-        //printf("Record: %s\n", line);
-        
-        char *token = strtok(line, SEPARATOR);
-        student_record record = { .id = 0, .lastname = "", .firstname = "", .grade = 0. };
-        int j = 0;
-        while (token != NULL) {
-            //printf("%s\n", token);
-            if (line_num != 0) {
-                int status = update_record(token, j, &record);
-                if (status == -1)
-                    printf("Failed to update some records\n");
-            }
-            token = strtok(NULL, SEPARATOR);
-            ++j;
-        }
+    int read_status = read_database(database);
 
-        if (line_num != 0) print_record(&record);
-        ++line_num;
+    if (read_status == -1) {
+        printf("Error occured with database read...");
+        return read_status;
     }
-
-    free(line);
 
     printf("\n");
 
