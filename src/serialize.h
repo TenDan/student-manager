9bc14bc3ce75a87cb5a032fc829168e62c11aaff
Maciej Ślusarz <mslusarz@student.agh.edu.pl>
2023-07-22 13:31:53 +0200

    Add serialization functions declarations

---
+++ src/serialize.h
@@ -0,0 +1,25 @@
+#ifndef SERIALIZE_H_
+#define SERIALIZE_H_
+
+#include <stdio.h>
+#include <string.h>
+
+#include "types.h"
+
+#define RECORD_BUFFER 1000
+
+/**
+ * Serializes record structure into string value
+ * @param source Student's record struct pointer
+ * @return Result string, NULL if error occured
+*/
+char* serialize_record(student_record* source);
+
+/**
+ * Deserialize string into student record struct
+ * @param source Raw string that should be deserializable
+ * @return Student record pointer, that will receive values from raw string, NULL if error occured
+*/
+student_record* deserialize_record(char* source);
+
+#endif /* SERIALIZE_H_ */
